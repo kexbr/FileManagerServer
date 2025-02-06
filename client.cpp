1907eb0f46f28bce8c5d9c7cd583af526168cf72
kexbr1234 <kexbr1234@gmail.com>
2025-02-06 03:17:59 +0300

    rewrote commands with func paradigma in client command handler. Echo command added.

--- client.cpp
+++ client.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Network.hpp>
+#include <chrono>
 #include <cstdint>
 #include <iostream>
 #include <mutex>
@@ -57,7 +58,7 @@ bool GetData(sf::TcpSocket* socket, sf::Packet& packet) {
 class Data {
    public:
     std::string ip_ = "127.0.0.1";
-    int port = 1330;
+    uint16_t port_ = 1330;
     std::string login_ = "admin";
     std::string pass_ = "admin";
     sf::TcpSocket* socket_ = nullptr;
@@ -69,33 +70,154 @@ void PrintMessage(std::string msg) {
     cout_mutex.unlock();
 }
 
+void TerminateSocket(Data& data) {
+    data.socket_->disconnect();
+    delete data.socket_;
+    data.socket_ = nullptr;
+}
+
+void HelpFunc(Data& data) {
+    PrintMessage(
+        "Command list:\nhelp - get command list\nlogin - configure login\npassword - "
+        "configure password\nip - configure server ip\nport - configure server port\necho "
+        "- echo connected server\n");
+    PrintMessage(
+        "connect - connect to the server with ip " + data.ip_ + " and port " +
+        std::to_string(data.port_) + "\n");
+    return;
+}
+
+void LoginChangeFunc(Data& data) {
+    PrintMessage(
+        "Current login is " + data.login_ +
+        ".\nEnter new login (or just press ENTER, if won't change login)\n");
+    std::string new_login;
+    getline(std::cin, new_login);
+    if (new_login.size() != 0) {
+        data.login_ = new_login;
+    }
+    return;
+}
+
+void PasswordChangeFunc(Data& data) {
+    PrintMessage(
+        "Current password is " + data.pass_ +
+        ".\nEnter new password (or just press ENTER, if won't change password)\n");
+    std::string new_pass;
+    getline(std::cin, new_pass);
+    if (new_pass.size() != 0) {
+        data.pass_ = new_pass;
+    }
+    return;
+}
+
+void IpChangeFunc(Data& data) {
+    PrintMessage(
+        "Current ip is " + data.ip_ +
+        ".\nEnter new ip (or just press ENTER, if won't change ip)\n");
+    std::string new_ip;
+    getline(std::cin, new_ip);
+    if (new_ip.size() != 0) {
+        data.ip_ = new_ip;
+    }
+    return;
+}
+
+void PortChangeFunc(Data& data) {
+    PrintMessage(
+        "Current port is " + std::to_string(data.port_) +
+        ".\nEnter new port (or just enter 0, if won't change port)\n");
+    uint16_t new_port;
+    std::cin >> new_port;
+    if (new_port != 0) {
+        data.port_ = new_port;
+    }
+    return;
+}
+
+bool ConnectToServer(Data& data, bool output_flag) {
+    if (output_flag) {
+        PrintMessage("Connecting to the server...");
+    }
+    data.socket_ = new sf::TcpSocket;
+    if (data.socket_->connect(data.ip_, data.port_) != sf::Socket::Done) {
+        if (output_flag) {
+            PrintMessage("Something went wrong during connection to the server!");
+        }
+        TerminateSocket(data);
+    } else {
+        if (output_flag) {
+            PrintMessage("Connected successfully!");
+        }
+        return true;
+    }
+    return false;
+}
+
+bool EchoFunc(Data& data, bool output_flag) {
+    if (data.socket_ == nullptr) {
+        if (output_flag) {
+            PrintMessage(
+                "There is no stable connection to the server! Use connect to connect to the "
+                "server");
+        }
+    } else {
+        sf::Packet packet;
+        packet << Queries::kEcho;
+        while (packet.getDataSize() < 64) {
+            packet << '0';
+        }
+        auto start_time = std::chrono::high_resolution_clock::now();
+        if (data.socket_->send(packet) != sf::Socket::Done) {
+            if (output_flag) {
+                PrintMessage("Something went wrong while sending the package. Disconnecting.");
+            }
+            TerminateSocket(data);
+        }
+        if (data.socket_->receive(packet) != sf::Socket::Done) {
+            if (output_flag) {
+                PrintMessage("Something went wrong while recieving the package. Disconnecting.");
+            }
+            TerminateSocket(data);
+        }
+        auto end_time = std::chrono::high_resolution_clock::now();
+        auto result = std::chrono::duration_cast<std::chrono::microseconds>(end_time - start_time);
+        if (output_flag) {
+            PrintMessage(
+                "Sent and recieved package with size " + std::to_string(packet.getDataSize()) +
+                " in " + std::to_string(result.count()) + " microseconds");
+        }
+        return true;
+    }
+    return false;
+}
+
 void CommandHandler() {
     Data data;
     PrintMessage("Client is ready. Type command \"help\" to get command list");
-    for(;;) {
+    for (;;) {
         std::string com;
         getline(std::cin, com);
         if (com == "help") {
-            PrintMessage("Command list:\nhelp - get command list\nlogin - configure login\npassword - configure password\nip - configure server ip\nport - configure server port");
-            continue;
+            HelpFunc(data);
         }
         if (com == "login") {
-            PrintMessage("Current login is " + data.login_ + ".\nEnter new login (or just press ENTER, if won't change login)");
-            std::string new_login;
-            getline(std::cin, new_login);
-            if (new_login.size() != 0) {
-                data.login_ = new_login;
-            }
-            continue;
+            LoginChangeFunc(data);
         }
         if (com == "password") {
-            PrintMessage("Current password is " + data.pass_ + ".\nEnter new password (or just press ENTER, if won't change password)");
-            std::string new_pass;
-            getline(std::cin, new_pass);
-            if (new_pass.size() != 0) {
-                data.pass_ = new_pass;
-            }
-            continue;
+            PasswordChangeFunc(data);
+        }
+        if (com == "ip") {
+            IpChangeFunc(data);
+        }
+        if (com == "port") {
+            PortChangeFunc(data);
+        }
+        if (com == "connect") {
+            ConnectToServer(data, true);
+        }
+        if (com == "echo") {
+            EchoFunc(data, true);
         }
     }
 }
